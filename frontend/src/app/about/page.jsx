32f58bc34fce09300009f02b6db7d0873b048441
Alisher <alisherxujanov163@gmail.com>
2025-03-13 10:50:12 +0500

    About page

---
+++ frontend/src/app/about/page.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam numquam deleniti quidem fugiat inventore excepturi quam voluptates fuga nisi in unde, iste, commodi perspiciatis nesciunt consequuntur temporibus! Delectus, consequatur inventore?</p>
+        </div>
+    )
+}
+
+export default About;
