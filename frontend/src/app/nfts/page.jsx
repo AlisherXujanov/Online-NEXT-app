405661cdb1c0bbbcea95e12f1f389956769d12e1
Alisher <alisherxujanov163@gmail.com>
2025-03-23 14:49:21 +0500

    update

--- frontend/src/app/nfts/page.jsx
+++ frontend/src/app/nfts/page.jsx
@@ -14,21 +14,54 @@ function NFTs(props) {
     // HOOKS
     // ...
     const [memory, setMemory] = useState("Hello world")
+    const [rangeInp, setRangeInput] = useState(50)
     // ---------------
     // all other staff
     // ...
     function handleChange(e) {
-        // const { value } = e.target
-        // setMemory(value)
-        setMemory(e.target.value)
+        const { value, name } = e.target
+        if (name === "memory") {
+            setMemory(value)
+        } else if (name === "range") {
+            setRangeInput(value)
+        }
     }
+
+    const boxStyle = {
+        padding: "20px",
+        borderRadius: "10px",
+        textAlign: "center",
+        backgroundColor: "lightgray",
+        margin: "10px"
+    }
+
     return (
         <div className="nfts-page page-wrapper">
             <Heading size={2}>NFTs Page</Heading>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, in expedita consequuntur, veritatis odit voluptas, harum similique optio mollitia sed architecto dolore delectus! Est mollitia natus, optio ut impedit molestias.</p>
-            <hr />
-            <h3>{memory}</h3>
-            <input type="text" onChange={handleChange}/>
+
+            <div className="box" style={boxStyle}>
+                <h3>{memory}</h3>
+                <input 
+                    type="text" 
+                    onChange={handleChange} 
+                    name='memory'
+                    value={memory}
+                />
+            </div>
+            <div className="box" style={boxStyle}>
+                <h2>Range: {rangeInp}</h2>
+                <input 
+                    type="range" 
+                    name='range'
+                    onChange={handleChange}
+                    value={rangeInp}
+                />
+            </div>
+            <div className="box" style={boxStyle}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                <input type="color" />
+            </div>
         </div>
     )
 }
