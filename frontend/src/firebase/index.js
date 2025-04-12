6058a35a1d14f13381ac2004721a74b2bdc727ea
Alisher <alisherxujanov163@gmail.com>
2025-04-12 13:19:17 +0500

    firebase

---
+++ frontend/src/firebase/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVuuh6HcfkecUiKj1xzqQ2uEJhYL-wP4Q",
+  authDomain: "online-group-12ccc.firebaseapp.com",
+  projectId: "online-group-12ccc",
+  storageBucket: "online-group-12ccc.firebasestorage.app",
+  messagingSenderId: "12096723221",
+  appId: "1:12096723221:web:74f77b863bc8c4a3dc711c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
